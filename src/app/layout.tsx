7f72c17f0546c5381bc9a4ffc4cc6d361def9ed0
kehinde adetule <portableewaade@gmail.com>
2025-01-09 10:30:13 +0100

    fix: trying out the export metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,18 @@ import Footer from '@/components/common/Footer';
 export const metadata: Metadata = {
   title: 'Ibidun Layi Ojo',
   description: "Welcome to Ibidun Layi Ojo's official website.",
+  openGraph: {
+    title: 'Ibidun Layi Ojo',
+    description: "Welcome to Ibidun Layi Ojo's official website.",
+    url: 'https://yourdomain.com', // Change to your actual domain
+    images: [
+      { url: 'https://yourdomain.com/assets/about-banner.png' }, // Your image for preview
+    ],
+    siteName: 'Ibidun Layi Ojo',
+  },
+  twitter: {
+    card: 'summary_large_image',
+  },
 };
 
 export default function RootLayout({
