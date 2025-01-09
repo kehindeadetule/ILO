805ef9baf06ae9ac1de912c1000c2e6e11c11cf2
kehinde adetule <portableewaade@gmail.com>
2025-01-09 17:14:55 +0100

    fix: change blog

--- src/app/blog/[id]/layout.tsx
+++ src/app/blog/[id]/layout.tsx
@@ -4,10 +4,11 @@ import {
   defaultMetaTags,
   pageMetaTags,
 } from '@/components/utils/config/metaTags';
+import { ReactNode } from 'react';
 
 interface BlogLayoutProps {
-  children: React.ReactNode;
-  params: { id: string } | Promise<{ id: string }>;
+  children: ReactNode;
+  params: { id: string };
 }
 
 interface Blog {
