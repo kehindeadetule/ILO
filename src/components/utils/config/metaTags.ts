52c9fa5d2b620ea393dd928066f95f96da0cf468
Kehinde Adetule <kehinde.adetule@theacconsulting.com>
2025-01-15 11:45:14 +0100

    feat: use relative path for image

--- src/components/utils/config/metaTags.ts
+++ src/components/utils/config/metaTags.ts
@@ -1,7 +1,76 @@
+// export const defaultMetaTags = {
+//   siteName: 'Ibidun Layi Ojo',
+//   defaultAuthor: 'Ibidun Layi Ojo',
+//   defaultImage: '/assets/contact-bg.png',
+//   defaultKeywords: 'blog, technology, web development',
+//   defaultDescription: "Welcome to Ibidun Layi Ojo's official website",
+// };
+
+// export const pageMetaTags = {
+//   home: {
+//     title: 'Home | Ibidun Layi Ojo',
+//     description:
+//       "Welcome to Ibidun Layi Ojo's official website. Explore my writings, media appearances, books, and more.",
+//     image: '/assets/banner-img.png',
+//     type: 'website',
+//   },
+//   about: {
+//     title: 'About | Ibidun Layi Ojo',
+//     description:
+//       'Learn more about Ibidun Layi Ojo, my journey, experiences, and what drives me.',
+//     image: '/assets/show-banner.png',
+//     type: 'website',
+//   },
+//   blog: {
+//     title: 'Blog | Ibidun Layi Ojo',
+//     description:
+//       'Read my latest blog posts, articles, and thoughts on various topics.',
+//     image: '/assets/show-banner.png',
+//     type: 'website',
+//   },
+//   media: {
+//     title: 'Media | Ibidun Layi Ojo',
+//     description:
+//       'Check out my media appearances, interviews, and public speaking engagements.',
+//     image: '/assets/media-hero.png',
+//     type: 'website',
+//   },
+//   books: {
+//     title: 'Books | Ibidun Layi Ojo',
+//     description: 'Explore my published books and literary works.',
+//     image: '/assets/contact-img.jpg',
+//     type: 'website',
+//   },
+//   contact: {
+//     title: 'Contact | Ibidun Layi Ojo',
+//     description:
+//       'Get in touch with me for inquiries, collaborations, or speaking engagements.',
+//     image: '/assets/contact-bg.png',
+//     type: 'website',
+//   },
+//   booking: {
+//     title: 'Booking | Ibidun Layi Ojo',
+//     description:
+//       'Get in touch with me for inquiries, collaborations, or speaking engagements.',
+//     image: '/assets/contact-hero.png',
+//     type: 'website',
+//   },
+//   login: {
+//     title: 'Login | Ibidun Layi Ojo',
+//     description: 'Authentication for comments.',
+//     image: '/assets/contact-hero.png',
+//     type: 'website',
+//   },
+// };
+
+
+// const baseUrl = 'https://blog.ibidunlayiojo.com';
+const baseUrl = 'https://ibidun-layi-ojo.vercel.app';
+
 export const defaultMetaTags = {
   siteName: 'Ibidun Layi Ojo',
   defaultAuthor: 'Ibidun Layi Ojo',
-  defaultImage: '/assets/contact-bg.png',
+  defaultImage: `${baseUrl}/assets/contact-bg.png`,
   defaultKeywords: 'blog, technology, web development',
   defaultDescription: "Welcome to Ibidun Layi Ojo's official website",
 };
@@ -11,54 +80,54 @@ export const pageMetaTags = {
     title: 'Home | Ibidun Layi Ojo',
     description:
       "Welcome to Ibidun Layi Ojo's official website. Explore my writings, media appearances, books, and more.",
-    image: '/assets/banner-img.png',
+    image: `${baseUrl}/assets/banner-img.png`,
     type: 'website',
   },
   about: {
     title: 'About | Ibidun Layi Ojo',
     description:
       'Learn more about Ibidun Layi Ojo, my journey, experiences, and what drives me.',
-    image: '/assets/show-banner.png',
+    image: `${baseUrl}/assets/show-banner.png`,
     type: 'website',
   },
   blog: {
     title: 'Blog | Ibidun Layi Ojo',
     description:
       'Read my latest blog posts, articles, and thoughts on various topics.',
-    image: '/assets/show-banner.png',
+    image: `${baseUrl}/assets/show-banner.png`,
     type: 'website',
   },
   media: {
     title: 'Media | Ibidun Layi Ojo',
     description:
       'Check out my media appearances, interviews, and public speaking engagements.',
-    image: '/assets/media-hero.png',
+    image: `${baseUrl}/assets/media-hero.png`,
     type: 'website',
   },
   books: {
     title: 'Books | Ibidun Layi Ojo',
     description: 'Explore my published books and literary works.',
-    image: '/assets/contact-img.jpg',
+    image: `${baseUrl}/assets/contact-img.jpg`,
     type: 'website',
   },
   contact: {
     title: 'Contact | Ibidun Layi Ojo',
     description:
       'Get in touch with me for inquiries, collaborations, or speaking engagements.',
-    image: '/assets/contact-bg.png',
+    image: `${baseUrl}/assets/contact-bg.png`,
     type: 'website',
   },
   booking: {
     title: 'Booking | Ibidun Layi Ojo',
     description:
       'Get in touch with me for inquiries, collaborations, or speaking engagements.',
-    image: '/assets/contact-hero.png',
+    image: `${baseUrl}/assets/contact-hero.png`,
     type: 'website',
   },
   login: {
     title: 'Login | Ibidun Layi Ojo',
     description: 'Authentication for comments.',
-    image: '/assets/contact-hero.png',
+    image: `${baseUrl}/assets/contact-hero.png`,
     type: 'website',
   },
 };
